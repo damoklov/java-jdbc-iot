a104980871f007addc3671302f633b0ae52cd70d
damoklov <mishanya@protonmail.com>
2020-11-02 18:39:26 +0200

    Added fallback view layer

---
+++ src/main/java/com/pazyniuk/passive/view/Printable.java
@@ -0,0 +1,6 @@
+package com.pazyniuk.passive.view;
+
+@FunctionalInterface
+public interface Printable {
+    void print();
+}
---
+++ src/main/java/com/pazyniuk/passive/view/View.java
@@ -0,0 +1,5 @@
+package com.pazyniuk.passive.view;
+
+public class View {
+
+}
